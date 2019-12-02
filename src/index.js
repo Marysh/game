8b37baeb2506e9a0f78daaf8b72640b3cedac9c9
marySh <sheremeta.m@gmail.com>
2019-12-02 18:53:13 +0200

    initial

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './audioService'
+import {Howl, Howler} from 'howler';
 
 class Wrapper extends React.Component {
   render() {
@@ -15,34 +16,27 @@ class Wrapper extends React.Component {
 
 
 class Toolbar extends React.Component {
+  state = {
+    audios: [],
+  };
+
+  handleOnDropAudio = (droppedAudios) => {
+    const {audios} = this.state;
+    this.setState({audios: [...audios, ...droppedAudios]})
+  };
+
   render() {
+    const {audios} = this.state;
+
     return (
       <div className="wrapper">
         <div className="toolbar">Drop your mp3 below</div>
-        <DropArea></DropArea>
-        <MediaContainer></MediaContainer>
-      </div>
-    )
-  }
-}
-
-
-class MediaContainer extends React.Component {
-  render() {
-    return (
-      <div>
-        {/*add map method to service[]*/}
-        <div className="media-wrapper">
-          <div>audio.name</div>
-          <div className="d-flex">
-            <button className="media-icon">
-              {/*<img src="https://icon-library.net/images/play-icon-svg/play-icon-svg-15.jpg" alt="play"/>*/}
-            </button>
-            <button className="media-icon">
-              {/*<img src="https://www.pngrepo.com/download/176023/music-pause-button-pair-of-lines.png" alt="pause"/>*/}
-            </button>
-          </div>
-        </div>
+        <DropArea onDrop={this.handleOnDropAudio}></DropArea>
+        {
+          audios.map((audio, index) => {
+            return <MediaContainer key={index} audio={audio}></MediaContainer>
+          })
+        }
       </div>
     )
   }
@@ -50,115 +44,20 @@ class MediaContainer extends React.Component {
 
 
 class DropArea extends React.Component {
+  constructor(props) {
+    super(props);
+    this.dropArea = React.createRef();
+  }
+
   componentDidMount() {
-    document.addEventListener("dragenter", (event) => {
-      if (event.target.id === "dropArea") {
-        event.target.style.opacity = "0.5";
-      }
-
-    }, false);
-
-
-    document.addEventListener("drop", function (event) {
-      event.preventDefault();
-      if (event.target.className === "dropArea") {
-        event.target.style.opacity = "1";
-        event.target.parentNode.removeChild(event.target);
-        event.target.appendChild(event.target);
-      }
-
-    }, false);
-
-
-    let context = new (window.AudioContext || window.webkitAudioContext)();
-    let source;
-    let processor;
-    let analyser;
-    let xhr;
-
-    function initAudio(data) {
-      source = context.createBufferSource();
-
-      if (context.decodeAudioData) {
-        context.decodeAudioData(data, function (buffer) {
-          source.buffer = buffer;
-          createAudio();
-        }, function (e) {
-          console.log(e);
-        });
-      } else {
-        source.buffer = context.createBuffer(data, false /*mixToMono*/);
-        createAudio();
-      }
-
-
-    }
-
-    function createAudio() {
-      processor = context.createScriptProcessor(2048 /*bufferSize*/, 1 /*num inputs*/, 1 /*num outputs*/);
-
-      analyser = context.createAnalyser();
-
-      source.connect(context.destination);
-      source.connect(analyser);
-
-      analyser.connect(processor);
-      processor.connect(context.destination);
-
-      source.start(0);
-      setTimeout(disconnect, source.buffer.duration * 1000 + 1000);
-    }
-
-    function disconnect() {
-      source.stop(0);
-      source.disconnect(0);
-      processor.disconnect(0);
-      analyser.disconnect(0);
-    }
+    const {onDrop} = this.props;
 
     function dropEvent(evt) {
       evt.stopPropagation();
       evt.preventDefault();
-
-      let droppedFiles = evt.dataTransfer.files;
-
-
-      // let formData = new FormData();
-      //
-      // for (let i = 0; i < droppedFiles.length; ++i) {
-      //   let file = droppedFiles[i];
-      //
-      //   files.append(file.name, file);
-      // }
-      //
-      // xhr = new XMLHttpRequest();
-      // xhr.open("POST", settings.url);
-      // xhr.onreadystatechange = handleResult;
-      // xhr.send(formData);
-
-
-      let reader = new FileReader();
-
-      reader.onload = function (fileEvent) {
-        let data = fileEvent.target.result;
-        initAudio(data);
-      };
-
-      reader.readAsArrayBuffer(droppedFiles[0]);
+      onDrop(evt.dataTransfer.files);
+      evt.target.style.opacity = "1";
     }
-
-    // function handleResult() {
-    //     if (xhr.readyState == 4 /* complete */) {
-    //         switch (xhr.status) {
-    //             case 200: /* Success */
-    //                 initAudio(request.response);
-    //                 break;
-    //             default:
-    //                 break;
-    //         }
-    //         xhr = null;
-    //     }
-    // }
 
     function dragOver(evt) {
       evt.stopPropagation();
@@ -166,22 +65,140 @@ class DropArea extends React.Component {
       return false;
     }
 
-    let dropArea = document.getElementById('dropArea');
-    dropArea.addEventListener('drop', dropEvent, false);
-    dropArea.addEventListener('dragover', dragOver, false);
-
+    this.dropArea.current.addEventListener('drop', dropEvent, false);
+    this.dropArea.current.addEventListener('dragover', dragOver, false);
+    this.dropArea.current.addEventListener("dragenter", (event) => {
+      event.target.style.opacity = "0.5";
+    }, false);
   }
 
   render() {
     return (
       <div>
-        <div id="dropArea">
+        <div className="dropArea" ref={this.dropArea}>
           <div>Drop here</div>
         </div>
       </div>
     )
   }
+
+  // componentWillUnmount() {
+  //   this.dropArea.current.removeEventListener();
+  //   this.dropArea.current.removeEventListener();
+  //   this.dropArea.current.removeEventListener();
+  // }
 }
+
+class MediaContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.playAudio = this.playAudio.bind(this);
+    this.pauseAudio = this.pauseAudio.bind(this);
+  }
+
+  componentDidMount() {
+    this.context = new (window.AudioContext || window.webkitAudioContext)();
+    this.reader = new FileReader();
+    let self = this;
+
+    this.reader.onload = function () {
+      let arrayBuffer = this.result;
+      self.initAudio(arrayBuffer);
+    };
+
+    this.reader.readAsArrayBuffer(this.props.audio);
+
+    //
+    // function createAudio() {
+    //   this.processor = this.context.createScriptProcessor(2048, 1, 1);
+    //
+    //   this.analyser = this.context.createAnalyser();
+    //
+    //   this.source.connect(this.context.destination);
+    //   this.source.connect(this.analyser);
+    //
+    //   this.analyser.connect(this.processor);
+    //   this.processor.connect(this.context.destination);
+    //
+    //   this.source.start(0);
+    //   setTimeout(disconnect, this.source.buffer.duration * 1000 + 1000);
+    // }
+    //
+    // function disconnect() {
+    //   this.source.stop(0);
+    //   this.source.disconnect(0);
+    //   this.processor.disconnect(0);
+    //   this.analyser.disconnect(0);
+    // }
+  }
+
+  disconnect = () => {
+    this.source.stop(0);
+    this.source.disconnect(0);
+    this.processor.disconnect(0);
+    this.analyser.disconnect(0);
+  }
+
+  createAudio = () => {
+    this.processor = this.context.createScriptProcessor(2048, 1, 1);
+    this.analyser = this.context.createAnalyser();
+    this.source.connect(this.context.destination);
+    this.source.connect(this.analyser);
+    this.analyser.connect(this.processor);
+    this.processor.connect(this.context.destination);
+    // this.source.start();
+    // this.source.context.suspend();
+    // setTimeout(this.disconnect, this.source.buffer.duration * 1000 + 1000);
+  }
+
+  initAudio = (data) => {
+    this.source = this.context.createBufferSource();
+
+    if (this.context.decodeAudioData) {
+      this.context.decodeAudioData(data, (buffer) => {
+        this.source.buffer = buffer;
+        this.createAudio();
+      }, (e) => {
+        console.log(e);
+      });
+    } else {
+      this.source.buffer = this.context.createBuffer(data, false /*mixToMono*/);
+      this.createAudio();
+    }
+  }
+
+  playAudio() {
+    // todo
+    // if (this.source.context.state === "running") {
+    //   this.source.resume();
+    // } else {
+    //   this.source.start();
+    // }
+  }
+
+  pauseAudio() {
+    this.source.context.suspend();
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="media-wrapper">
+          <div>{this.props.audio.name}</div>
+          <div className="d-flex">
+            <div className="media-icon" onClick={this.playAudio}>
+              <img src="https://icon-library.net/images/play-icon-svg/play-icon-svg-15.jpg" alt="play"/>
+            </div>
+            <div className="media-icon" onClick={this.pauseAudio}>
+              <img src="https://www.pngrepo.com/download/176023/music-pause-button-pair-of-lines.png" alt="pause"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
 
 ReactDOM.render(<Wrapper/>, document.getElementById("root"));
 
